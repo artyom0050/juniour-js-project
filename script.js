6e90c095f33f8f8ef9b43a5bbab730ca669c42ac
Artyom <adyan.artyom@icloud.com>
2021-01-02 14:42:12 +0400

    add elememt for,while

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let money = prompt("Ваш бюджет на месяц", ''),
+let money = +prompt("Ваш бюджет на месяц", ''),
     time = prompt('Внесите дату в формате YYYY-MM-DD', '');
 
 let appData  = {
@@ -10,12 +10,34 @@ let appData  = {
     savings: false
 };
 
-let a1 = prompt("ВВедите обязательную статью расходов в этом месяце", ''),
-    a2 = prompt("Во скллько обойдется?", ''),
-    a3 = prompt("ВВедите обязательную статью расходов в этом месяце", ''),
-    a4 = prompt("Во скллько обойдется?", '');
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
 
-alert(appData.budget / 30);
+for (let i = 0; i < 2; i++) {
+    let a = prompt("ВВедите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во скллько обойдется?", '');
+
+    if ( (typeof(a)) === 'string' && (typeof(a)) != null  && (typeof(b)) != null
+    && a != '' && b != '' && a.length < 50) {
+        console.log("done");
+
+        appData.expenses[a] = b;
+    } else {
+        console.log("bad result");
+        i--;
+    }
+
+};
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Daily budget:" + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log("minimum level profit");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("midle level profit");
+}else if (appData.moneyPerDay > 2000) {
+    console.log("High level profit")
+}else {
+    console.log("Mistake")
+}
